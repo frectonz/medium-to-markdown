fa08984174be89af927e9d87a22c2c6ed8c61584
frectonz <fraol0912@gmail.com>
2024-04-30 00:38:57 +0300

    add flake

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, crane }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        craneLib = crane.lib.${system};
+        src = craneLib.cleanCargoSource ./.;
+
+        mainBuildInputs = with pkgs; [
+          openssl
+          pkg-config
+        ];
+
+        commonArgs = {
+          inherit src;
+          buildInputs = mainBuildInputs;
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        bin = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+        });
+      in
+      with pkgs;
+      {
+        packages = {
+          inherit bin;
+          default = bin;
+        };
+
+        devShells.default = mkShell {
+          buildInputs = mainBuildInputs ++ [
+            rust-bin.stable.latest.default
+            rust-analyzer
+          ];
+        };
+
+        formatter = nixpkgs-fmt;
+      }
+    );
+}
